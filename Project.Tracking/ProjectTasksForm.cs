de159d15eb3b2158a498c07a8bd7b061c940384a
thecelestialjewel <christinemjordan6@gmail.com>
2020-12-05 16:24:37 -0800

    Start to set up form with dataset from main.

--- Project.Tracking/ProjectTasksForm.cs
+++ Project.Tracking/ProjectTasksForm.cs
@@ -12,9 +12,15 @@ namespace Project.Tracking
 {
     public partial class ProjectTasksForm : Form
     {
+        //Receiving dataset from mainform and updating controls to use dataset.
         public ProjectTasksForm(ProjectTrackingDataSet projectTrackingDataSet)
         {
             InitializeComponent();
+            this.projectTrackingDataSet = null;
+            this.projectTrackingDataSet = projectTrackingDataSet;
+            this.projectBindingSource.DataSource = this.projectTrackingDataSet;
+
+
         }
 
         //Getting main form to access public methods and properities.
